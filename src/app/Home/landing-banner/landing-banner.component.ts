f17babcbae5e18c41abaebca0dad618a8e6ed73a
saurabsen <saurav540@yahoo.com>
2021-04-18 21:14:05 +0530

    new chnages

---
+++ src/app/Home/landing-banner/landing-banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-banner',
+  templateUrl: './landing-banner.component.html',
+  styleUrls: ['./landing-banner.component.scss']
+})
+export class LandingBannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopnavbarComponent } from './Navigation/topnavbar/topnavbar.component';
+import { LandingBannerComponent } from './Home/landing-banner/landing-banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopnavbarComponent,
+    LandingBannerComponent
   ],
   imports: [
     BrowserModule,
