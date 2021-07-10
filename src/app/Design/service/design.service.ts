531aff0a7d2eaced74fc0d89ce14b8399a1925ed
saurabsenneudesic <saurab.sen@neudesic.com>
2021-07-10 15:09:43 +0530

    ../../../assets to assets

--- src/app/Design/service/design.service.ts
+++ src/app/Design/service/design.service.ts
@@ -10,62 +10,62 @@ export class DesignService {
   constructor(private http: HttpClient) {}
 
   public getAccessibility(): Observable<any>{
-    return this.http.get("../../../assets/Json-files/accessibility.json");
+    return this.http.get("assets/Json-files/accessibility.json");
   }
 
   public getAnimation(): Observable<any>{
-    return this.http.get("../../../assets/Json-files/animation.json");
+    return this.http.get("assets/Json-files/animation.json");
   }
 
   public getBooks(): Observable<any>{
-    return this.http.get("../../../assets/Json-files/books.json");
+    return this.http.get("assets/Json-files/books.json");
   }
 
   public getColor(): Observable<any>{
-    return this.http.get("../../../assets/Json-files/color.json");
+    return this.http.get("assets/Json-files/color.json");
   }
 
   public getContent(): Observable<any>{
-    return this.http.get("../../../assets/Json-files/content.json");
+    return this.http.get("assets/Json-files/content.json");
   }
 
   public getDesignSystem(): Observable<any>{
-    return this.http.get("../../../assets/Json-files/design-system.json");
+    return this.http.get("assets/Json-files/design-system.json");
   }
 
   public getGradient(): Observable<any>{
-    return this.http.get("../../../assets/Json-files/gradient.json");
+    return this.http.get("assets/Json-files/gradient.json");
   }
 
   public getIcon(): Observable<any>{
-    return this.http.get("../../../assets/Json-files/icon.json");
+    return this.http.get("assets/Json-files/icon.json");
   }
 
   public getIllustration(): Observable<any>{
-    return this.http.get("../../../assets/Json-files/illustration.json");
+    return this.http.get("assets/Json-files/illustration.json");
   }
 
   public getMockup(): Observable<any>{
-    return this.http.get("../../../assets/Json-files/mockup.json");
+    return this.http.get("assets/Json-files/mockup.json");
   }
 
   public getInspiration(): Observable<any>{
-    return this.http.get("../../../assets/Json-files/inspiration.json");
+    return this.http.get("assets/Json-files/inspiration.json");
   }
 
   public getPatternAndAbstract(): Observable<any>{
-    return this.http.get("../../../assets/Json-files/pattern-and-abstract.json");
+    return this.http.get("assets/Json-files/pattern-and-abstract.json");
   }
 
   public getImage(): Observable<any>{
-    return this.http.get("../../../assets/Json-files/stock-image.json");
+    return this.http.get("assets/Json-files/stock-image.json");
   }
 
   public getVideo(): Observable<any>{
-    return this.http.get("../../../assets/Json-files/video-image.json");
+    return this.http.get("assets/Json-files/video-image.json");
   }
 
   public getTypography(): Observable<any>{
-    return this.http.get("../../../assets/Json-files/typography.json");
+    return this.http.get("assets/Json-files/typography.json");
   }
 }
